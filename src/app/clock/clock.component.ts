2b424b265596ba16da90663ecc925548ebe80f01
. <anusha.l.anusha@capgemini.com>
2025-01-10 14:49:55 +0530

    third commit

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -2,31 +2,20 @@ import { Component, OnInit,OnDestroy } from '@angular/core';
 import { Observable, interval, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import  moment from 'moment';
+import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-clock',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './clock.component.html',
   styleUrl: './clock.component.css'
 })
-export class ClockComponent implements OnInit, OnDestroy{
-  currentTime: string = '';
-  timeSubscription: Subscription | null = null;
+export class ClockComponent {
 
-  ngOnInit(): void {
-    const timeObservable: Observable<string> = interval(1000).pipe(
-      map(() => moment().format('h:mm:ss A'))
-    );
+  currentTime: Observable<string> = interval(1000).pipe(
+    map(() => moment().format('h:mm:ss A'))
+  );
 
-    this.timeSubscription = timeObservable.subscribe((time: string) => {
-      this.currentTime = time;
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.timeSubscription) {
-      this.timeSubscription.unsubscribe();
-    }
-  }
 }
